bdfecb9e9bebcd598d307e1e32639dd8daa9a9a3
Patrick <patrickaasamoah@gmail.com>
2024-12-14 23:55:21 -0600

    .

--- myapp/src/index.js
+++ myapp/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import {
   BrowserRouter, Routes, Route
 } from "react-router-dom"
+import './index.css';
+import Home from './pages/Home/Home';
+import About from "./pages/About/About";
+import BookList from "./components/BookList/BookList";
+import BookDetails from "./components/BookDetails/BookDetails"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <></>
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path = "/" element = {<Home/>}>
+
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
 
